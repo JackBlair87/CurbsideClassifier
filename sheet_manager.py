605f4b06e63237613aecba7973a8b8f6166d3dbd
Akash Pamal <akashpamal@gmail.com>
2021-04-11 15:49:42 -0400

    cleaned up repo

--- sheet_manager.py
+++ sheet_manager.py
@@ -17,6 +17,7 @@ class SheetManager():
 
     def add_order(self, space_number, car_type, order_number):
         # print(sheet.get_all_values())
+        print('The order number is', order_number)
         row = [space_number, car_type, order_number]
         index = 2
         self.sheet.insert_row(row, index)
