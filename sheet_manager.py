e9e94af97319f784be0e605509bf299c2c4d211e
Akash Pamal <akashpamal@gmail.com>
2021-04-11 14:51:20 -0400

    finished developing classifier, automatically writes to Google Sheets. but where the order number is always 50

---
+++ sheet_manager.py
@@ -0,0 +1,29 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import random
+
+class SheetManager():
+    # use creds to create a client to interact with the Google Drive API
+    def __init__(self) -> None:
+        scope = ['https://spreadsheets.google.com/feeds',
+                'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        client = gspread.authorize(creds)
+
+        # Find a workbook by name and open the first sheet
+        # Make sure you use the right name here.
+        self.sheet = client.open("Curbside Classifier").sheet1
+        # self.sheet.clear()
+
+    def add_order(self, space_number, car_type, order_number):
+        # print(sheet.get_all_values())
+        row = [space_number, car_type, order_number]
+        index = 2
+        self.sheet.insert_row(row, index)
+        # self.sheet.clear
+    
+    def add_scene(self, cars_in_spaces):
+        self.sheet.clear()
+        self.sheet.insert_row(["Space number", "Car type", "Order number"], 1)
+        for car in cars_in_spaces:
+            self.add_order(1, car, str(random.randrange(0, 1000)))
